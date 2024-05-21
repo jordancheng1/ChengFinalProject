2f578782ecf51b7dc9af1936291acdbb7137af40
BT_3W2_12 <jordanc249@nycstudents.net>
2024-05-21 09:27:19 -0400

    Added Aang

--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -14,32 +14,41 @@ public class WelcomePanel extends JPanel {
     private JFrame enclosingFrame;
     private BufferedImage selectButton;
     private BufferedImage characterSelect;
+    private BufferedImage character1;
     private double xCoord = 50;
     private double yCoord = 50;
+    private String charName;
+    private boolean charSelected = false;
 
     public WelcomePanel(JFrame frame) {
         enclosingFrame = frame;
         try {
             selectButton = ImageIO.read(new File("src/SelectButton.png"));
             characterSelect = ImageIO.read(new File("src/CharacterSelect.png"));
+            character1 = ImageIO.read(new File("src/Aang.png"));
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
-
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(selectButton, 50, 50, null);
+        if (charSelected) {
+            g.drawImage(selectButton, 50, 50, null);
+        }
         g.drawImage(characterSelect, 100, 0, null);
+        g.drawImage(character1, 100, 0, null);
     }
 
     public void mouseReleased(MouseEvent e) {
-        if (selectRect().contains(e.getPoint())) {
-            MainFrame f = new MainFrame();
+        if (selectRect().contains(e.getPoint()) && !charName.isBlank()) {
+            MainFrame f = new MainFrame(charName);
             enclosingFrame.setVisible(false);
+        }
+        else if (character1Rect().contains(e.getPoint())) {
+            charName = "The Last Avatar";
+            charSelected = true;
         }
     }
 
@@ -47,5 +56,11 @@ public class WelcomePanel extends JPanel {
         int selectButtonHeight = selectButton.getHeight();
         int selectButtonWidth = selectButton.getWidth();
         return new Rectangle((int) xCoord, (int) yCoord, selectButtonWidth, selectButtonHeight);
+    }
+
+    public Rectangle character1Rect() {
+        int character1Height = character1.getHeight();
+        int character1Width = character1.getWidth();
+        return new Rectangle((int) xCoord, (int) yCoord, character1Width, character1Height);
     }
 }
